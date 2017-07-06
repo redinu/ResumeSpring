8bc428a7a7234f40612488440b4ae29109cdf56c
Rediet Ayalew <rediet2000@gmail.com>
2017-07-06 02:13:25 -0400

    data loader added

--- src/main/java/com/resume/spring/security/DataLoader.java
+++ src/main/java/com/resume/spring/security/DataLoader.java
@@ -1,4 +1,4 @@
-/*package com.resume.spring.security;
+package com.resume.spring.security;
 
 import java.util.Arrays;
 
@@ -52,4 +52,3 @@ public class DataLoader implements CommandLineRunner{
 
     }
 }
-*/
